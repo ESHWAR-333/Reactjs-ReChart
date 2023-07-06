27a04d383d169f70b0c3e6370215ecb1e5d4ab54
ESHWAR <chalamallan3@gmail.com>
2023-07-06 12:08:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import BarGraph from './components/BarGraph/index'
+import PieGraph from './components/PieGraph/index'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=() => {
+  return(
+    <>
+<PieGraph/>
+<BarGraph/>
+    </>
+  )
+
 }
 
 export default App;
